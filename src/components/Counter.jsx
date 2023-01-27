6689d7e9ff9c683c1da4e59cfa790b27c076bb3a
Gen <kebikovgen@gmail.com>
2023-01-28 02:34:24 +0300

    code finish lesson-7

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,18 @@
+import {dec, inc, rnd} from '../actions';
 
-const Counter = ({inc, dec, rnd, count}) => {
+import { useSelector, useDispatch } from "react-redux";
+//useSelector амтоматом получает state
+
+const Counter = () => {
+    const value = useSelector(state => state.value);
+    const dispatch = useDispatch();
 
     return(
         <div className="jumbotron">
-            <h1>{ count }</h1>
-            <button className="btn btn-primary" onClick={dec} >DEC</button>
-            <button className="btn btn-primary" onClick={inc} >INC</button>
-            <button className="btn btn-primary" onClick={() => rnd(56)} >RND</button>
+            <h1>{value}</h1>
+            <button onClick={() => dispatch(dec())} className="btn btn-primary">DEC</button>
+            <button onClick={() => dispatch(inc())} className="btn btn-primary">INC</button>
+            <button onClick={() => dispatch(rnd())} className="btn btn-primary">RND</button>
         </div>
     )
 }
